ab3b77a9ebdfbf381cf68b384d89bce4c90cf579
AllanFerreiraGomes <allanferreiragomes1@gmail.com>
2024-11-12 19:27:28 -0300

    inicio

---
+++ projeto-naf-back/src/test/java/com/example/projeto_naf_back/ProjetoNafBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projeto_naf_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoNafBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
